b6a6fdfc4ed28be0473e98681fcd87c1317fee8d
MarkDestroyer <markkir123@gmail.com>
2023-06-14 00:04:57 +0300

    update
    Added icons for sections.

--- AGPU/AGPU/Models/AGPUSections.swift
+++ AGPU/AGPU/Models/AGPUSections.swift
@@ -9,14 +9,14 @@ import Foundation
 
 struct AGPUSections {
     static let sections = [
-        AGPUSectionModel(id: 0, name: "Главное", subsections: AGPUSubSections.subsections["Главное"]!, voiceCommand: "главное"),
-        AGPUSectionModel(id: 1, name: "Университет", subsections: AGPUSubSections.subsections["Университет"]!, voiceCommand: "универ"),
-        AGPUSectionModel(id: 2, name: "Абитуриенту", subsections: AGPUSubSections.subsections["Абитуриенту"]!, voiceCommand: "абитуриент"),
-        AGPUSectionModel(id: 3, name: "Студенту", subsections: AGPUSubSections.subsections["Студенту"]!, voiceCommand: "студент"),
-        AGPUSectionModel(id: 4, name: "Сотрудникам", subsections: AGPUSubSections.subsections["Сотрудникам"]!, voiceCommand: "сотрудник"),
-        AGPUSectionModel(id: 5, name: "Наука", subsections: AGPUSubSections.subsections["Наука"]!, voiceCommand: "наук"),
-    AGPUSectionModel(id: 6, name: "Партнерам", subsections: AGPUSubSections.subsections["Партнерам"]!, voiceCommand: "партнёр"),
-        AGPUSectionModel(id: 7, name: "Международная деятельность", subsections: AGPUSubSections.subsections["Международная деятельность"]!, voiceCommand: "отдел международной деятельности"),
-        AGPUSectionModel(id: 8, name: "Безопасность", subsections: AGPUSubSections.subsections["Безопасность"]!, voiceCommand: "безопасность")
+        AGPUSectionModel(id: 0, name: "Главное", icon: "home ", subsections: AGPUSubSections.subsections["Главное"]!, voiceCommand: "главное"),
+        AGPUSectionModel(id: 1, name: "Университет", icon: "university", subsections: AGPUSubSections.subsections["Университет"]!, voiceCommand: "универ"),
+        AGPUSectionModel(id: 2, name: "Абитуриенту", icon: "user", subsections: AGPUSubSections.subsections["Абитуриенту"]!, voiceCommand: "абитуриент"),
+        AGPUSectionModel(id: 3, name: "Студенту", icon: "student", subsections: AGPUSubSections.subsections["Студенту"]!, voiceCommand: "студент"),
+        AGPUSectionModel(id: 4, name: "Сотрудникам", icon: "people", subsections: AGPUSubSections.subsections["Сотрудникам"]!, voiceCommand: "сотрудник"),
+        AGPUSectionModel(id: 5, name: "Наука", icon: "science", subsections: AGPUSubSections.subsections["Наука"]!, voiceCommand: "наук"),
+        AGPUSectionModel(id: 6, name: "Партнерам", icon: "paper plane", subsections: AGPUSubSections.subsections["Партнерам"]!, voiceCommand: "партнёр"),
+        AGPUSectionModel(id: 7, name: "Международная деятельность", icon: "world", subsections: AGPUSubSections.subsections["Международная деятельность"]!, voiceCommand: "отдел международной деятельности"),
+        AGPUSectionModel(id: 8, name: "Безопасность", icon: "security", subsections: AGPUSubSections.subsections["Безопасность"]!, voiceCommand: "безопасность")
     ]
 }
