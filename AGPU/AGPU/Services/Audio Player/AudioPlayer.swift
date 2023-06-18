b88a91afdc33773f39025fa92f25ba21d5a59534
MarkDestroyer <markkir123@gmail.com>
2023-06-18 22:14:50 +0300

    update
    fixed bugs.

---
+++ AGPU/AGPU/Services/Audio Player/AudioPlayer.swift
@@ -0,0 +1,54 @@
+//
+//  AudioPlayer.swift
+//  AGPU
+//
+//  Created by Марк Киричко on 18.06.2023.
+//
+
+import AVFoundation
+
+class AudioPlayer {
+    
+    var player: AVAudioPlayer?
+    
+    static let shared = AudioPlayer()
+    
+    func PlaySound(resource: String) {
+        
+        guard let url = Bundle.main.url(forResource: resource, withExtension: nil) else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func StopSound(resource: String) {
+
+        guard let url = Bundle.main.url(forResource: resource, withExtension: nil) else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.stop()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
+
