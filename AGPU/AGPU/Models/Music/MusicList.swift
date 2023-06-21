8ee20f4d1537276ecd493c47f3c9a72f7ad0c03a
MarkDestroyer <markkir123@gmail.com>
2023-06-21 22:36:46 +0300

    update
    Added MVVM for settings.

---
+++ AGPU/AGPU/Models/Music/MusicList.swift
@@ -0,0 +1,19 @@
+//
+//  Music.swift
+//  AGPU
+//
+//  Created by Марк Киричко on 21.06.2023.
+//
+
+import Foundation
+
+struct MusicList {
+    
+    static var musicArray = [
+        MusicModel(id: 1, name: "информатика", fileName: "informatics music.mp3", isChecked: false),
+        MusicModel(id: 2, name: "литература", fileName: "literature music.mp3", isChecked: false),
+        MusicModel(id: 3, name: "спорт", fileName: "sport music.mp3", isChecked: false),
+        MusicModel(id: 4, name: "история", fileName: "history music.mp3", isChecked: false),
+        MusicModel(id: 5, name: "дошкола", fileName: "pre school music.mp3", isChecked: false)
+    ]
+}
