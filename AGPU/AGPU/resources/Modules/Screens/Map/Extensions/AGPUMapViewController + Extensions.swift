035b96a3816b220263b1780dfbeeb67afe099631
MarkDestroyer <markkir123@gmail.com>
2023-06-30 00:21:26 +0300

    update
    Added notifications for maps.

--- AGPU/AGPU/resources/Modules/Screens/Map/Extensions/AGPUMapViewController + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Screens/Map/Extensions/AGPUMapViewController + Extensions.swift
@@ -24,19 +24,28 @@ extension AGPUMapViewController: CLLocationManagerDelegate {
 extension AGPUMapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        NotificationCenter.default.post(name: Notification.Name("Map Pin Selected"), object: nil)
         if let annotation = view.annotation
         {
             let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: annotation.coordinate, addressDictionary: nil))
             mapItem.name = annotation.title ?? ""
             let yes = UIAlertAction(title: "да", style: .default) { _ in
-                mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
+                NotificationCenter.default.post(name: Notification.Name("Go To Map"), object: nil)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
+                    NotificationCenter.default.post(name: Notification.Name("Map Was Opened"), object: nil)
+                }
             }
-            let cancel = UIAlertAction(title: "нет", style: .default) { _ in}
-            self.ShowAlert(
-                title: "Вы хотите начать путь до: \"\(mapItem.name ?? "")\"?",
-                message: "",
-                actions: [cancel, yes]
-            )
+            let cancel = UIAlertAction(title: "нет", style: .default) { _ in
+                NotificationCenter.default.post(name: Notification.Name("Map Pin Cancelled"), object: nil)
+            }
+            Timer.scheduledTimer(withTimeInterval: 1.5, repeats: false) { _ in
+                self.ShowAlert(
+                    title: "Вы хотите начать путь до: \"\(mapItem.name ?? "")\"?",
+                    message: "",
+                    actions: [cancel, yes]
+                )
+            }
         }
     }
 }
