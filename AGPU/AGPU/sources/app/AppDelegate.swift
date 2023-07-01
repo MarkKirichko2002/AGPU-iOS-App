1cb16bffb6ca17fcf9b2e6b3186b31d5930cccc8
MarkDestroyer <markkir123@gmail.com>
2023-07-01 23:34:45 +0300

    update
    Fixed bug.

--- AGPU/AGPU/sources/app/AppDelegate.swift
+++ AGPU/AGPU/sources/app/AppDelegate.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        configureAudioSession()
         return true
     }
     
