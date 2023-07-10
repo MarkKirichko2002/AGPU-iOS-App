9498f364950ba483170c591d66d897dc6d7ce294
MarkDestroyer <markkir123@gmail.com>
2023-07-11 00:28:23 +0300

    update
    Added loading indicator on screen "Recent page".

--- AGPU/AGPU/resources/Modules/Screens/Recent Page/Extensions/RecentPageViewController + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Screens/Recent Page/Extensions/RecentPageViewController + Extensions.swift
@@ -10,10 +10,17 @@ import WebKit
 // MARK: - WKNavigationDelegate
 extension RecentPageViewController: WKNavigationDelegate {
     
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        DispatchQueue.main.async {
+            self.spinner.startAnimating()
+        }
+    }
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.spinner.stopAnimating()
         if let page = UserDefaults.loadData(type: RecentPageModel.self, key: "last page") {
             if webView.url?.absoluteString == page.url {
-                DispatchQueue.main.async {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                     self.WVWEBview.scrollView.setContentOffset(page.position, animated: true)
                 }
             }
