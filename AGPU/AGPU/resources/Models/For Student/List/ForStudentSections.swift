4de48af08ef912ba64f196202345d310b2be185a
MarkDestroyer <markkir123@gmail.com>
2023-07-12 00:39:29 +0300

    update
    Added section "Campus and hostels" on screen "For student".

--- AGPU/AGPU/resources/Models/For Student/List/ForStudentSections.swift
+++ AGPU/AGPU/resources/Models/For Student/List/ForStudentSections.swift
@@ -10,7 +10,8 @@ import Foundation
 struct ForStudentSections {
     static let sections = [
         ForStudentModel(id: 1, name: "расписание", icon: ""),
-        ForStudentModel(id: 2, name: "карты", icon: ""),
-        ForStudentModel(id: 3, name: "факультеты и кафедры", icon: "")
+        ForStudentModel(id: 2, name: "найти «АГПУ»", icon: ""),
+        ForStudentModel(id: 3, name: "факультеты и кафедры", icon: ""),
+        ForStudentModel(id: 4, name: "кампус и общежития", icon: ""),
     ]
 }
