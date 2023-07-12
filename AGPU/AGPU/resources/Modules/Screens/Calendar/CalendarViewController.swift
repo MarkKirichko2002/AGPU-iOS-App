7182ab9523cf713170d879ad691bf00585cbeb62
MarkDestroyer <markkir123@gmail.com>
2023-07-12 04:27:00 +0300

    update
    Added calendar.

---
+++ AGPU/AGPU/resources/Modules/Screens/Calendar/CalendarViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CalendarViewController.swift
+//  AGPU
+//
+//  Created by Марк Киричко on 12.07.2023.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController {
+
+    private let Calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(Calendar)
+        Calendar.delegate = self
+        makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        NSLayoutConstraint.activate([
+            Calendar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            Calendar.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            Calendar.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            Calendar.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+}
+
+// MARK: - FSCalendarDelegate
+extension CalendarViewController: FSCalendarDelegate {
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        let date = formatter.string(from: date)
+        NotificationCenter.default.post(name: Notification.Name("DateWasSelected"), object: date)
+        self.dismiss(animated: true)
+    }
+}
