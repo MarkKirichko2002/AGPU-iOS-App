2e177e8355977e30a2c1cd72ba1a7e8925872bf0
MarkDestroyer <markkir123@gmail.com>
2023-07-12 06:51:44 +0300

    update
    Added option to choose group in settings.

--- AGPU/AGPU/resources/Modules/Clean Architecture/Settings MVVM/View/Extensions/SettingsListViewController + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/Settings MVVM/View/Extensions/SettingsListViewController + Extensions.swift
@@ -6,3 +6,42 @@
 //
 
 import UIKit
+
+// MARK: - UITableViewDataSource
+extension SettingsListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return AGPUGroups.groups.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = viewModel.isGroupSelected(index: indexPath.row)
+        cell.tintColor = .systemGreen
+        cell.textLabel?.text = AGPUGroups.groups[indexPath.row]
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SettingsListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.ChangeGroup(index: indexPath.row)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "выберите группу"
+        default:
+            return ""
+        }
+    }
+}
