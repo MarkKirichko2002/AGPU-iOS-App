04ac1838bf9e0ecda562f1df54c9c5a85260b875
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-07-17 05:57:20 +0300

    update
    Added detail screen for AGPU buildings location.

---
+++ AGPU/AGPU/resources/Modules/Screens/AGPU Location Detail Screen/View/ViewController/AGPULocationDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AGPULocationDetailViewController.swift
+//  AGPU
+//
+//  Created by Марк Киричко on 17.07.2023.
+//
+
+import UIKit
+import MapKit
+
+class AGPULocationDetailViewController: UIViewController {
+    
+    var annotation: MKAnnotation!
+    
+    @IBOutlet var LocationName: UILabel!
+    @IBOutlet var LocationDetail: UILabel!
+    
+    // MARK: - Init
+    init(annotation: MKAnnotation) {
+        self.annotation = annotation
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        LocationName.text = annotation.title!
+        LocationDetail.text = annotation.subtitle!
+    }
+    
+    @IBAction func GoToMap() {
+        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: annotation.coordinate, addressDictionary: nil))
+        mapItem.name = annotation.title ?? ""
+        mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
+    }
+}
+
