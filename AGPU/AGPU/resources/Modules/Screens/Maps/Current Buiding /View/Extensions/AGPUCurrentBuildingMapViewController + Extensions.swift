e20c077bb720ee8d00bd5919fb05653f6494892f
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-07-22 00:03:37 +0300

    update
    Refactored code.

--- AGPU/AGPU/resources/Modules/Screens/Maps/Current Buiding /View/Extensions/AGPUCurrentBuildingMapViewController + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Screens/Maps/Current Buiding /View/Extensions/AGPUCurrentBuildingMapViewController + Extensions.swift
@@ -5,19 +5,7 @@
 //  Created by Марк Киричко on 18.07.2023.
 //
 
-import CoreLocation
 import MapKit
-
-// MARK: - CLLocationManagerDelegate
-extension AGPUCurrentBuildingMapViewController: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            manager.stopUpdatingLocation()
-            render(location)
-        }
-    }
-}
 
 // MARK: - MKMapViewDelegate
 extension AGPUCurrentBuildingMapViewController: MKMapViewDelegate {
