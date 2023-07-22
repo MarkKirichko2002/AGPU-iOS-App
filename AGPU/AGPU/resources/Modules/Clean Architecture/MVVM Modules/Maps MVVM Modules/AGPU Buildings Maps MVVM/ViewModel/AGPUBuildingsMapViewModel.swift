4791d92b2316c60851cf41006249ee3b4946371f
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-07-23 02:24:42 +0300

    update
    Added MVVM for AGPU buildings map.

---
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Maps MVVM Modules/AGPU Buildings Maps MVVM/ViewModel/AGPUBuildingsMapViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  AGPUBuildingsMapViewModel.swift
+//  AGPU
+//
+//  Created by Марк Киричко on 23.07.2023.
+//
+
+import MapKit
+
+class AGPUBuildingsMapViewModel {
+    
+    private var locationHandler: ((LocationModel)->Void)?
+    
+    // MARK: - сервисы
+    private let locationManager = LocationManager()
+    
+    func registerLocationHandler(block: @escaping(LocationModel)->Void) {
+        self.locationHandler = block
+    }
+    
+    func GetLocation() {
+        
+        locationManager.GetLocations()
+        
+        locationManager.registerLocationHandler { location in
+            
+            let coordinate = CLLocationCoordinate2D(
+                latitude: location.coordinate.latitude,
+                longitude: location.coordinate.longitude
+            )
+            
+            let span = MKCoordinateSpan(
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1
+            )
+            
+            let region = MKCoordinateRegion(
+                center: coordinate,
+                span: span
+            )
+            
+            // текущая геопозиция
+            let currentpin = MKPointAnnotation()
+            currentpin.coordinate = coordinate
+            currentpin.title = "Вы"
+            
+            AGPUPins.pins.append(currentpin)
+            
+            let location = LocationModel(region: region, pins: AGPUPins.pins)
+            
+            self.locationHandler?(location)
+            
+        }
+    }
+}
