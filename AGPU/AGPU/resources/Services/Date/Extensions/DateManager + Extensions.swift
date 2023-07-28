b4d733a0063a15663fa7f4a1e260c1b2db9ebb0b
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-07-28 18:15:22 +0300

    update
    Added options in menu on timetable screen to choose next previous day.

--- AGPU/AGPU/resources/Services/Date/Extensions/DateManager + Extensions.swift
+++ AGPU/AGPU/resources/Services/Date/Extensions/DateManager + Extensions.swift
@@ -23,4 +23,42 @@ extension DateManager: DateManagerProtocol {
         let timeString = dateFormatter.string(from: date)
         return timeString
     }
+    
+    func nextDay(date: String)-> String {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        var nextDay = ""
+        
+        if let date = dateFormatter.date(from: date) {
+            let calendar = Calendar.current
+            var dateComponent = DateComponents()
+            dateComponent.day = 1
+            
+            if let tomorrow = calendar.date(byAdding: dateComponent, to: date) {
+                let tomorrowString = dateFormatter.string(from: tomorrow)
+                nextDay = tomorrowString
+            }
+        }
+        return nextDay
+    }
+    
+    func previousDay(date: String)-> String {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        var previousDay = ""
+        
+        if let date = dateFormatter.date(from: date) {
+            let calendar = Calendar.current
+            var dateComponent = DateComponents()
+            dateComponent.day = -1
+            
+            if let yesterday = calendar.date(byAdding: dateComponent, to: date) {
+                let yesterdayString = dateFormatter.string(from: yesterday)
+                previousDay = yesterdayString
+            }
+        }
+        return previousDay
+    }
 }
