e690a8b2b8c798a6d0cfdd287672811bc466ca86
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-07-29 23:58:07 +0300

    update
    Added full adress for every cathedra.

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Faculty Cathedra List/Model/Faculty Сathedra/FacultyCathedraList.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Faculty Cathedra List/Model/Faculty Сathedra/FacultyCathedraList.swift
@@ -15,7 +15,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра русского языка, литературы и методики их преподавания",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-ryalimip/index.php",
-                address: "ул.Р.Люксембург, 159",
+                address: "ул.Р.Люксембург, 159, главн. корпус ФГБОУ ВО АГПУ",
                 email: "ofizh@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-ryalimip/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-ryalimip/dopolnitelnoe-obrazovanie/index.php"
@@ -23,7 +23,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра иностранных языков и методики их преподавания",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/index.php",
-                address: "ул. Р. Люксембург 159",
+                address: "ул. Р. Люксембург 159, кабинет №12",
                 email: "angl_kaf_agpu@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/dopolnitelnoe-obrazovanie/index.php"
@@ -34,7 +34,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра математики, физики и методики их преподавания",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-mfimip/index.php",
-                address: "ул. Р.Люксембург, 159",
+                address: "ул. Р.Люксембург, 159, ауд. 17",
                 email: "arctgx@yandex.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-mfimip/dopolnitelnoe-obrazovanie/index.php"
@@ -42,7 +42,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра информатики и информационных технологий обучения",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-infiITO/index.php",
-                address: "ул. Р. Люксембург 159",
+                address: "Адрес кафедры: ул. Р. Люксембург 159, кабинет №13.",
                 email: "agpu_kaf_inf@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-infiITO/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-infiITO/dopolnitelnoe-obrazovanie/index.php"
@@ -72,7 +72,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра теории, истории педагогики и образовательной практики",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-tipiop/index.php",
-                address: "ул. Р. Люксембург 159",
+                address: "ул. Р. Люксембург 159, кабинет №15",
                 email: "kaf_tipiop@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-tipiop/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-tipiop/dopolnitelnoe-obrazovanie/index.php"
@@ -80,7 +80,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра педагогики и технологий дошкольного и начального образования",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-pitdino/index.php",
-                address: "ул. Р. Люксембург 159",
+                address: "ул. Р. Люксембург 159, кабинет №1",
                 email: "kaf_pitdino@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-pitdino/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-pitdino/dopolnitelnoe-obrazovanie/index.php"
@@ -91,7 +91,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра технологии и дизайна",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-tid/index.php",
-                address: "ул. Комсомольская, 93",
+                address: "ул. Комсомольская, 93, кабинет 55а",
                 email: "agpu_ofap@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-tid/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-tid/dopolnitelnoe-obrazovanie/index.php"
@@ -118,7 +118,7 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра философии, права и социально-гуманитарных наук",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-fpisgn/index.php",
-                address: "ул. Володарского, д. 120 б",
+                address: "ул. Володарского, д. 120б",
                 email: "kaffilosof@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-fpisgn/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-fpisgn/dopolnitelnoe-obrazovanie/index.php"
