42d450dfa019bc8f579ec12e1c54f9fd34913a2d
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-07-30 00:13:32 +0300

    update
    Added coordinates for every cathedra.

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Faculty Cathedra List/Model/Faculty Сathedra/FacultyCathedraList.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Faculty Cathedra List/Model/Faculty Сathedra/FacultyCathedraList.swift
@@ -16,6 +16,10 @@ struct FacultyCathedraList {
                 name: "Кафедра русского языка, литературы и методики их преподавания",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-ryalimip/index.php",
                 address: "ул.Р.Люксембург, 159, главн. корпус ФГБОУ ВО АГПУ",
+                coordinates: [
+                    45.001817,
+                    41.132393
+                ],
                 email: "ofizh@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-ryalimip/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-ryalimip/dopolnitelnoe-obrazovanie/index.php"
@@ -24,6 +28,10 @@ struct FacultyCathedraList {
                 name: "Кафедра иностранных языков и методики их преподавания",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/index.php",
                 address: "ул. Р. Люксембург 159, кабинет №12",
+                coordinates: [
+                    45.001817,
+                    41.132393
+                ],
                 email: "angl_kaf_agpu@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/dopolnitelnoe-obrazovanie/index.php"
@@ -35,6 +43,10 @@ struct FacultyCathedraList {
                 name: "Кафедра математики, физики и методики их преподавания",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-mfimip/index.php",
                 address: "ул. Р.Люксембург, 159, ауд. 17",
+                coordinates: [
+                    45.001817,
+                    41.132393
+                ],
                 email: "arctgx@yandex.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/iriif/kafedra-iyaimip/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-mfimip/dopolnitelnoe-obrazovanie/index.php"
@@ -42,7 +54,11 @@ struct FacultyCathedraList {
             FacultyCathedraModel(
                 name: "Кафедра информатики и информационных технологий обучения",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-infiITO/index.php",
-                address: "Адрес кафедры: ул. Р. Люксембург 159, кабинет №13.",
+                address: "ул. Р. Люксембург 159, кабинет №13.",
+                coordinates: [
+                    45.001817,
+                    41.132393
+                ],
                 email: "agpu_kaf_inf@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-infiITO/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/ipimif/kafedra-infiITO/dopolnitelnoe-obrazovanie/index.php"
@@ -54,6 +70,10 @@ struct FacultyCathedraList {
                 name: "Кафедра социальной, специальной педагогики и психологии",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/spf/kafedra-sspip/index.php",
                 address: "ул. Ленина, 79",
+                coordinates: [
+                    45.002263,
+                    41.121873
+                ],
                 email: "sspp2010@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/spf/kafedra-sspip/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/spf/kafedra-sspip/dopolnitelnoe-obrazovanie/index.php"
@@ -62,6 +82,10 @@ struct FacultyCathedraList {
                 name: "Кафедра физической культуры и медико-биологических дисциплин",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/spf/kafedra-fkimbd/index.php",
                 address: "ул. П. Осипенко, 83/1",
+                coordinates: [
+                    45.006374,
+                    41.128629
+                ],
                 email: "andrmaz@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/spf/kafedra-fkimbd/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/spf/kafedra-fkimbd/dopolnitelnoe-obrazovanie/index.php"
@@ -73,6 +97,10 @@ struct FacultyCathedraList {
                 name: "Кафедра теории, истории педагогики и образовательной практики",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-tipiop/index.php",
                 address: "ул. Р. Люксембург 159, кабинет №15",
+                coordinates: [
+                    45.001817,
+                    41.132393
+                ],
                 email: "kaf_tipiop@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-tipiop/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-tipiop/dopolnitelnoe-obrazovanie/index.php"
@@ -81,6 +109,10 @@ struct FacultyCathedraList {
                 name: "Кафедра педагогики и технологий дошкольного и начального образования",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-pitdino/index.php",
                 address: "ул. Р. Люксембург 159, кабинет №1",
+                coordinates: [
+                    45.001817,
+                    41.132393
+                ],
                 email: "kaf_pitdino@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-pitdino/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fdino/kafedra-pitdino/dopolnitelnoe-obrazovanie/index.php"
@@ -92,6 +124,10 @@ struct FacultyCathedraList {
                 name: "Кафедра технологии и дизайна",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-tid/index.php",
                 address: "ул. Комсомольская, 93, кабинет 55а",
+                coordinates: [
+                    45.003697,
+                    41.122763
+                ],
                 email: "agpu_ofap@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-tid/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-tid/dopolnitelnoe-obrazovanie/index.php"
@@ -100,6 +136,10 @@ struct FacultyCathedraList {
                 name: "Кафедра экономики и управления",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-eiu/index.php",
                 address: "ул. Комсомольская, 93",
+                coordinates: [
+                    45.003697,
+                    41.122763
+                ],
                 email: "pshmahova_m@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-eiu/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/fteid/kafedra-eiu/dopolnitelnoe-obrazovanie/index.php"
@@ -111,6 +151,10 @@ struct FacultyCathedraList {
                 name: "Кафедра всеобщей и отечественной истории",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-vioi/index.php",
                 address: "ул. Володарского 120б",
+                coordinates: [
+                    44.989082,
+                    41.126904
+                ],
                 email: "kvioi-agpu@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-vioi/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-vioi/dopolnitelnoe-obrazovanie/index.php"
@@ -119,6 +163,10 @@ struct FacultyCathedraList {
                 name: "Кафедра философии, права и социально-гуманитарных наук",
                 url: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-fpisgn/index.php",
                 address: "ул. Володарского, д. 120б",
+                coordinates: [
+                    44.989082,
+                    41.126904
+                ],
                 email: "kaffilosof@mail.ru",
                 manualUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-fpisgn/MetodicheskoyeObespecheniye/index.php",
                 additionalEducationUrl: "http://test.agpu.net/struktura-vuza/faculties-institutes/istfak/kafedra-fpisgn/dopolnitelnoe-obrazovanie/index.php"
--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Faculty Cathedra List/Model/Model/FacultyCathedraModel.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Faculty Cathedra List/Model/Model/FacultyCathedraModel.swift
@@ -5,12 +5,13 @@
 //  Created by Марк Киричко on 24.07.2023.
 //
 
-import Foundation
+import MapKit
 
 struct FacultyCathedraModel: Codable {
     let name: String
     let url: String
     let address: String
+    let coordinates: [Double]
     let email: String
     let manualUrl: String
     let additionalEducationUrl: String
