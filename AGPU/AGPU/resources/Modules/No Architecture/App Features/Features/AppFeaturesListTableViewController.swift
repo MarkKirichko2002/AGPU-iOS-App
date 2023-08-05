f9daed11b942a2d326d8709072a9928e952b2d22
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-08-05 13:21:25 +0300

    update
    Fixed bug.

--- AGPU/AGPU/resources/Modules/No Architecture/App Features/Features/AppFeaturesListTableViewController.swift
+++ AGPU/AGPU/resources/Modules/No Architecture/App Features/Features/AppFeaturesListTableViewController.swift
@@ -16,6 +16,11 @@ final class AppFeaturesListTableViewController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.post(name: Notification.Name("screen was closed"), object: nil)
+    }
+    
     private func SetUpNavigation() {
         
         navigationItem.title = "Фишки"
