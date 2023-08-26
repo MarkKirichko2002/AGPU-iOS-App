aade07fdf18734fb8969ce196001e374dfb61db4
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-08-26 15:14:03 +0300

    Minor change.

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/News Modules/News List MVVM/View/Extensions/NewsListViewController + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/News Modules/News List MVVM/View/Extensions/NewsListViewController + Extensions.swift
@@ -10,28 +10,27 @@ import UIKit
 // MARK: - UICollectionViewDelegate
 extension NewsListViewController: UICollectionViewDelegate {
         
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+            if let cell = collectionView.cellForItem(at: indexPath) as? NewsCollectionViewCell {
+                cell.didTapCell(indexPath: indexPath)
+            }
+        }
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+            self.GoToWeb(url: "\(self.viewModel.makeUrlForCurrentArticle(index: indexPath.row))", image: "online", title: "\(self.viewModel.articleItem(index: indexPath.row).date ?? "")", isSheet: false)
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView,
                                  contextMenuConfigurationForItemAt indexPath: IndexPath,
                                  point: CGPoint) -> UIContextMenuConfiguration? {
         return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { suggestedActions in
-            
-            let infoAction = UIAction(title: "узнать больше", image: UIImage(named: "info")) { _ in
-                Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
-                    if let cell = collectionView.cellForItem(at: indexPath) as? NewsCollectionViewCell {
-                        cell.didTapCell(indexPath: indexPath)
-                    }
-                }
-                Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
-                    self.GoToWeb(url: "\(self.viewModel.makeUrlForCurrentArticle(index: indexPath.row))", image: "online", title: "\(self.viewModel.articleItem(index: indexPath.row).date ?? "")", isSheet: false)
-                }
-            }
             
             let shareAction = UIAction(title: "поделиться", image: UIImage(named: "share")) { _ in
                 self.shareInfo(image: UIImage(named: self.viewModel.faculty?.icon ?? "АГПУ")!, title: "\(self.viewModel.articleItem(index: indexPath.row).title ?? "")", text: "\(self.viewModel.makeUrlForCurrentArticle(index: indexPath.row))")
             }
             
             return UIMenu(title: self.viewModel.articleItem(index: indexPath.row).title ?? "", children: [
-                infoAction,
                 shareAction
             ])
         }
