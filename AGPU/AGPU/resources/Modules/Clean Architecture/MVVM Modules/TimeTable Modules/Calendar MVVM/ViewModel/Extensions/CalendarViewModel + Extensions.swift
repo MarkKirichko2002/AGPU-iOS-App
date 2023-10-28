a78665880b1c95642c053930cc0150e0fb3cf51b
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-10-28 13:00:57 +0300

    update
    minor improvements in "Smart Calendar".

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/TimeTable Modules/Calendar MVVM/ViewModel/Extensions/CalendarViewModel + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/TimeTable Modules/Calendar MVVM/ViewModel/Extensions/CalendarViewModel + Extensions.swift
@@ -16,8 +16,17 @@ extension CalendarViewModel: CalendarViewModelProtocol {
         service.getTimeTableDay(groupId: group, date: date) { [weak self] result in
             switch result {
             case .success(let data):
+                
+                if data.disciplines.contains(where: { $0.type == .hol }) {
+                    self?.timetableHandler?("Праздничный выходной", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(self?.dateManager.getCurrentDate() ?? "") занятий нет")
+                }
+                
+                if data.disciplines.contains(where: { $0.name.contains("практика") }) {
+                    self?.timetableHandler?("В этот день есть практика", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date) количество пар: \(self?.getPairsCount(pairs: data.disciplines) ?? 0)")
+                }
+                
                 if !data.disciplines.isEmpty {
-                    self?.timetableHandler?("У группы \(self?.group ?? "") есть расписание","\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date) количество пар: \(self?.getPairsCount(pairs: data.disciplines) ?? 0)")
+                    self?.timetableHandler?("У группы \(self?.group ?? "") есть расписание", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date) количество пар: \(self?.getPairsCount(pairs: data.disciplines) ?? 0)")
                 } else {
                     self?.timetableHandler?("У группы \(self?.group ?? "") нет расписания", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date) нет пар")
                 }
