e64d3126814d79547853e2fe47529e9ea1646608
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-11-29 01:52:06 +0300

    update
    Added option to share building location.

--- AGPU/AGPU/resources/Modules/No Architecture/Maps/AGPU Location Detail Screen/View/ViewController/AGPULocationDetailViewController.swift
+++ AGPU/AGPU/resources/Modules/No Architecture/Maps/AGPU Location Detail Screen/View/ViewController/AGPULocationDetailViewController.swift
@@ -35,12 +35,37 @@ final class AGPULocationDetailViewController: UIViewController {
     private func setUpNavigation() {
         navigationItem.title = "Найти «АГПУ»"
         let closeButton = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(closeScreen))
+        let shareButton = UIBarButtonItem(image: UIImage(named: "share"), style: .plain, target: self, action: #selector(showShareAlert))
         closeButton.tintColor = .label
-        navigationItem.rightBarButtonItem = closeButton
+        shareButton.tintColor = .label
+        navigationItem.leftBarButtonItem = closeButton
+        navigationItem.rightBarButtonItem = shareButton
     }
     
     @objc private func closeScreen() {
         dismiss(animated: true)
+    }
+    
+    @objc private func showShareAlert() {
+        let shareAppleMaps = UIAlertAction(title: "С помощью Apple Maps", style: .default) { _ in
+            self.shareLocationWithAppleMaps()
+        }
+        let shareGoogleMaps = UIAlertAction(title: "С помощью Google Maps", style: .default) { _ in
+            self.shareLocationWithGoogleMaps()
+        }
+        self.showAlert(title: "Поделиться локацией", message: "Как вы хотите поделиться локацией?", actions: [shareAppleMaps, shareGoogleMaps])
+    }
+    
+    private func shareLocationWithAppleMaps() {
+        let title = annotation.title!
+        let url = "http://maps.apple.com/?q=\(annotation.coordinate.latitude),\(annotation.coordinate.longitude)"
+        shareInfo(image: UIImage(named: "map icon")!, title: title!, text: "\(title!)-\(url)")
+    }
+    
+    private func shareLocationWithGoogleMaps() {
+        let title = annotation.title!
+        let url = "https://www.google.com/maps/search/?api=1&query=\(annotation.coordinate.latitude),\(annotation.coordinate.longitude)"
+        shareInfo(image: UIImage(named: "map icon")!, title: title!, text: "\(title!)-\(url)")
     }
     
     private func setUpView() {
