942904ecb48d00772f048c3d0b8f07556d512be2
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-11-30 06:49:28 +0300

    update
    minor changes.

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/TimeTable Modules/Calendar MVVM/View/ViewController/CalendarViewController.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/TimeTable Modules/Calendar MVVM/View/ViewController/CalendarViewController.swift
@@ -80,10 +80,13 @@ final class CalendarViewController: UIViewController {
             ])
             
             let choose = UIAlertAction(title: "Выбрать", style: .default) { _ in
+                HapticsManager.shared.hapticFeedback()
                 self.viewModel.sendNotificationDataWasSelected(date: self.viewModel.date)
                 self.dismiss(animated: true)
             }
-            let cancel = UIAlertAction(title: "Отмена", style: .destructive) { _ in}
+            let cancel = UIAlertAction(title: "Отмена", style: .destructive) { _ in
+                HapticsManager.shared.hapticFeedback()
+            }
             
             alertVC.setValue(customTitle, forKey: "attributedTitle")
             alertVC.addAction(choose)
