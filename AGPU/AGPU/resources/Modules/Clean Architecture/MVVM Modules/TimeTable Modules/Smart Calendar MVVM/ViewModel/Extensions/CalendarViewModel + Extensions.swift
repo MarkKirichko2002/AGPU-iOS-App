cb43ffe6578afab464d15c342716b5d8ecb6c5fd
MarkKirichko2002 <58703027+MarkKirichko2002@users.noreply.github.com>
2024-01-04 19:34:14 +0300

    update
    minor changes.

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/TimeTable Modules/Smart Calendar MVVM/ViewModel/Extensions/CalendarViewModel + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/TimeTable Modules/Smart Calendar MVVM/ViewModel/Extensions/CalendarViewModel + Extensions.swift
@@ -16,13 +16,13 @@ extension CalendarViewModel: CalendarViewModelProtocol {
         service.getTimeTableDay(groupId: group, date: date) { [weak self] result in
             switch result {
             case .success(let data):
-                AudioPlayerClass.shared.playSound(sound: "paper", isPlaying: false)
                 // выходной
                 if data.disciplines.contains(where: { $0.type == .hol }) {
                     self?.timetableHandler?("Праздничный выходной", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(self?.dateManager.getCurrentDate() ?? "") занятий нет", UIColor.systemGray)
+                    AudioPlayerClass.shared.playSound(sound: "paper", isPlaying: false)
                 }
                 // практика
-                if data.disciplines.contains(where: { $0.name.contains("практика") }) {
+                else if data.disciplines.contains(where: { $0.name.contains("практика") }) {
                     
                     let startTimes = data.disciplines[0].time.components(separatedBy: "-")
                     let startTime = startTimes[0]
@@ -31,9 +31,10 @@ extension CalendarViewModel: CalendarViewModelProtocol {
                     let endTime = endTimes[1]
                     
                     self?.timetableHandler?("В этот день есть практика!", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date), количество пар: \(self?.getPairsCount(pairs: data.disciplines) ?? 0), начало: \(startTime), конец: \(endTime)", UIColor.systemYellow)
+                    AudioPlayerClass.shared.playSound(sound: "paper", isPlaying: false)
                 }
                 // курсовая
-                if data.disciplines.contains(where: { $0.name.contains("курсов.") }) {
+                else if data.disciplines.contains(where: { $0.name.contains("курсов.") }) {
                     
                     let startTimes = data.disciplines[0].time.components(separatedBy: "-")
                     let startTime = startTimes[0]
@@ -51,7 +52,7 @@ extension CalendarViewModel: CalendarViewModelProtocol {
                     AudioPlayerClass.shared.playSound(sound: "danger", isPlaying: false)
                 }
                 // зачет
-                if data.disciplines.contains(where: { $0.type == .cred }) {
+                else if data.disciplines.contains(where: { $0.type == .cred }) {
                     
                     let startTimes = data.disciplines[0].time.components(separatedBy: "-")
                     let startTime = startTimes[0]
@@ -62,9 +63,10 @@ extension CalendarViewModel: CalendarViewModelProtocol {
                     let testsCount = self?.getTestsCount(pairs: data.disciplines) ?? 0
                     
                     self?.timetableHandler?("В этот день есть \(testsCount > 1 ? "зачёты!" : "зачёт!")", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date), пар: \(pairsCount), зачётов: \(testsCount), начало: \(startTime), конец: \(endTime)", UIColor.systemYellow)
+                    AudioPlayerClass.shared.playSound(sound: "paper", isPlaying: false)
                 }
                 // консультация
-                if data.disciplines.contains(where: { $0.type == .cons }) {
+                else if data.disciplines.contains(where: { $0.type == .cons }) {
                     
                     let startTimes = data.disciplines[0].time.components(separatedBy: "-")
                     let startTime = startTimes[0]
@@ -74,9 +76,10 @@ extension CalendarViewModel: CalendarViewModelProtocol {
                     let consCount = self?.getConsCount(pairs: data.disciplines) ?? 0
                     
                     self?.timetableHandler?("В этот день консультация!", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date), консультаций: \(consCount), \nначало: \(startTime), конец: \(endTime)", UIColor.systemYellow)
+                    AudioPlayerClass.shared.playSound(sound: "paper", isPlaying: false)
                 }
                 // экзамен
-                if data.disciplines.contains(where: { $0.type == .exam }) {
+                else if data.disciplines.contains(where: { $0.type == .exam }) {
                     
                     let startTimes = data.disciplines[0].time.components(separatedBy: "-")
                     let startTime = startTimes[0]
@@ -89,7 +92,7 @@ extension CalendarViewModel: CalendarViewModelProtocol {
                     AudioPlayerClass.shared.playSound(sound: "danger", isPlaying: false)
                 }
                 // расписание есть
-                if !data.disciplines.isEmpty {
+                else if !data.disciplines.isEmpty {
                     
                     let startTimes = data.disciplines[0].time.components(separatedBy: "-")
                     let startTime = startTimes[0]
@@ -99,9 +102,11 @@ extension CalendarViewModel: CalendarViewModelProtocol {
                     let pairsCount = self?.getPairsCount(pairs: data.disciplines) ?? 0
                     
                     self?.timetableHandler?("В этот день есть \(pairsCount > 1 ? "пары" : "пара")", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date), количество пар: \(pairsCount), начало: \(startTime), конец: \(endTime)", UIColor.systemGreen)
+                    AudioPlayerClass.shared.playSound(sound: "paper", isPlaying: false)
                 } else {
                 // расписания нет
                     self?.timetableHandler?("Расписание отсутствует", "\(self?.dateManager.getCurrentDayOfWeek(date: date) ?? "") \(date) нет пар", UIColor.systemGray)
+                    AudioPlayerClass.shared.playSound(sound: "paper", isPlaying: false)
                 }
             case .failure(let error):
                 print(error)
