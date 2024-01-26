13945b837898836aa3f3f7cead77bcd859bc0337
MarkKirichko2002 <58703027+MarkKirichko2002@users.noreply.github.com>
2024-01-26 18:03:16 +0300

    fixed bug.

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Settings Modules/Timetable Settings List MVVM/ViewModel/Extensions/TimetableSettingsListViewModel + Extensions.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Settings Modules/Timetable Settings List MVVM/ViewModel/Extensions/TimetableSettingsListViewModel + Extensions.swift
@@ -50,21 +50,19 @@ extension TimetableSettingsListViewModel: TimetableSettingsListViewModelProtocol
     
     func currentOwnerScreen()-> UIViewController {
         let owner = UserDefaults.standard.object(forKey: "recentOwner") as? String ?? "GROUP"
+        let searchVC = TimeTableSearchListTableViewController()
+        searchVC.isSettings = true
         if owner == "GROUP" {
             if let faculty = getSavedFaculty() {
                 let vc = FacultyGroupsListTableViewController(faculty: faculty)
                 return vc
             }
         } else if owner == "TEACHER" {
-            let vc = TimeTableSearchListTableViewController()
-            vc.isSettings = true
-            return vc
+            return searchVC
         } else if owner == "CLASSROOM" {
-            let vc = TimeTableSearchListTableViewController()
-            vc.isSettings = true
-            return vc
+            return searchVC
         }
-        return UIViewController()
+        return searchVC
     }
     
     func getSavedGroup()-> String {
