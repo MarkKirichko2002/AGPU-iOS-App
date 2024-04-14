89165d65e3403deecbb7cef3b7ac550c7399c1f6
MarkKirichko2002 <58703027+MarkKirichko2002@users.noreply.github.com>
2024-04-14 14:31:52 +0300

    minor changes.

--- AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Maps Modules/Cathedra/Cathedra Building Detail MVVM/View/ViewController/CathedraBuildingDetailViewController.swift
+++ AGPU/AGPU/resources/Modules/Clean Architecture/MVVM Modules/Maps Modules/Cathedra/Cathedra Building Detail MVVM/View/ViewController/CathedraBuildingDetailViewController.swift
@@ -23,10 +23,10 @@ class CathedraBuildingDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
-        setUpNavigation()
-        setUpWeatherLabel()
-        setUpTitleLabel()
         bindViewModel()
+        setUpNavigation()
+        setUpTitleLabel()
+        setUpWeatherLabel()
     }
     
     private func setUpView() {
@@ -37,44 +37,14 @@ class CathedraBuildingDetailViewController: UIViewController {
         WeatherLabel.textColor = UIColor.label
     }
     
-    private func setUpTitleLabel() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(showCathedraInfo))
-        LocationName.isUserInteractionEnabled = true
-        LocationName.addGestureRecognizer(tap)
-    }
-    
-    @objc private func showCathedraInfo() {
-        for faculty in AGPUFaculties.faculties {
-            for cathedra in faculty.cathedra {
-                if cathedra.name == annotation.title! {
-                    HapticsManager.shared.hapticFeedback()
-                    self.goToWeb(url: cathedra.url, image: faculty.icon, title: "Кафедра \(faculty.abbreviation)", isSheet: false, isNotify: false)
-                    break
-                }
-            }
-        }
-    }
-    
-    private func setUpWeatherLabel() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(showWeatherDetail))
-        WeatherLabel.isUserInteractionEnabled = true
-        WeatherLabel.addGestureRecognizer(tap)
-    }
-    
-    @objc private func showWeatherDetail() {
-        HapticsManager.shared.hapticFeedback()
-        let vc = LocationWeatherDetailViewController(annotation: annotation)
-        let navVC = UINavigationController(rootViewController: vc)
-        navVC.modalPresentationStyle = .fullScreen
-        present(navVC, animated: true)
-    }
-    
     private func setUpNavigation() {
-        navigationItem.title = "Найти кампус"
+        let info = viewModel.getInfo()
+        let titleView = CustomTitleView(image: info.1.icon, title: "Кафедра \(info.1.abbreviation)", frame: .zero)
         let closeButton = UIBarButtonItem(image: UIImage(named: "cross"), style: .plain, target: self, action: #selector(closeScreen))
         let shareButton = UIBarButtonItem(image: UIImage(named: "share"), style: .plain, target: self, action: #selector(showShareVC))
         closeButton.tintColor = .label
         shareButton.tintColor = .label
+        navigationItem.titleView = titleView
         navigationItem.leftBarButtonItem = closeButton
         navigationItem.rightBarButtonItem = shareButton
     }
@@ -89,6 +59,32 @@ class CathedraBuildingDetailViewController: UIViewController {
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true)
         HapticsManager.shared.hapticFeedback()
+    }
+    
+    private func setUpTitleLabel() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(showCathedraInfo))
+        LocationName.isUserInteractionEnabled = true
+        LocationName.addGestureRecognizer(tap)
+    }
+    
+    @objc private func showCathedraInfo() {
+        let info = viewModel.getInfo()
+        HapticsManager.shared.hapticFeedback()
+        self.goToWeb(url: info.0.url, image: info.1.icon, title: "Кафедра \(info.1.abbreviation)", isSheet: false, isNotify: false)
+    }
+    
+    private func setUpWeatherLabel() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(showWeatherDetail))
+        WeatherLabel.isUserInteractionEnabled = true
+        WeatherLabel.addGestureRecognizer(tap)
+    }
+    
+    @objc private func showWeatherDetail() {
+        HapticsManager.shared.hapticFeedback()
+        let vc = LocationWeatherDetailViewController(annotation: annotation)
+        let navVC = UINavigationController(rootViewController: vc)
+        navVC.modalPresentationStyle = .fullScreen
+        present(navVC, animated: true)
     }
     
     private func bindViewModel() {
